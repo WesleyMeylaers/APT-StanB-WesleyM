974c72c46398caafe6d34c9ff93f861901aa70ac
WesleyMeylaers <r0929439@student.thomasmore.be>
2024-12-11 12:19:41 +0100

    Push omdat chat da wil

--- api-gateway/src/main/java/com/example/apigateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/example/apigateway/config/SecurityConfig.java
@@ -2,12 +2,9 @@ package com.example.apigateway.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-
-import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebFluxSecurity
@@ -16,18 +13,11 @@ public class SecurityConfig {
     @Bean
     public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity serverHttpSecurity) {
         serverHttpSecurity
-                .authorizeExchange(exchange -> 
-                    // Open toegang voor de /user, /artist en /album routes
-                    exchange.pathMatchers(HttpMethod.GET, "/user/**", "/artist/**", "/album/**")
-                        .permitAll()
-                        // Bescherm toegang tot /playlist en andere routes, alleen geauthenticeerde gebruikers mogen toegang krijgen
-                        .pathMatchers("/playlist/**")
-                        .authenticated()
-                        .anyExchange()
-                        .authenticated()  // Alle andere eindpunten moeten geauthenticeerd zijn
-                )
-                .oauth2ResourceServer(oauth2 -> oauth2
-                    .jwt(withDefaults())  // JWT configuratie voor resource server
+                .authorizeExchange(exchange ->
+                        exchange.pathMatchers("/user/**", "/artist/**", "/album/**", "/playlist/**")
+                                .permitAll()  // Open toegang voor alle paden
+                                .anyExchange()
+                                .permitAll()  // Ook alle andere eindpunten open
                 );
         return serverHttpSecurity.build();
     }
