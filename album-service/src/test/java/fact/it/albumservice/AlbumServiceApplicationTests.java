385aa6b52032d7c2dbb4ff854613662969020a87
WesleyMeylaers <r0929439@student.thomasmore.be>
2024-12-18 13:11:54 +0100

    test

--- album-service/src/test/java/fact/it/albumservice/AlbumServiceApplicationTests.java
+++ album-service/src/test/java/fact/it/albumservice/AlbumServiceApplicationTests.java
@@ -1,24 +1,13 @@
 package fact.it.albumservice;
 
-import fact.it.albumservice.service.AlbumService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import fact.it.albumservice.dto.AlbumRequest;
-import fact.it.albumservice.dto.AlbumResponse;
-import fact.it.albumservice.model.Album;
-import fact.it.albumservice.repository.AlbumRepository;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
 
 @SpringBootTest
 class AlbumServiceApplicationTests {
-    
+
+    @Test
+    void contextLoads() {
+    }
+
 }
