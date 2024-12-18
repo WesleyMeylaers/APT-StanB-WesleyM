cf8aa97a14be0960adca3ab5f3c803ed0dea0a7a
WesleyMeylaers <r0929439@student.thomasmore.be>
2024-12-18 20:36:31 +0100

    test

--- user-service/src/test/java/fact/it/userservice/UserServiceApplicationTests.java
+++ user-service/src/test/java/fact/it/userservice/UserServiceApplicationTests.java
@@ -1,26 +1,13 @@
 package fact.it.userservice;
 
-import fact.it.userservice.service.UserService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import fact.it.userservice.dto.UserResponse;
-import fact.it.userservice.model.User;
-import fact.it.userservice.repository.UserRepository;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-
-import java.util.List;
-import java.util.Optional;
-
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
 
 @SpringBootTest
 class UserServiceApplicationTests {
+
     @Test
     void contextLoads() {
     }
+
 }
