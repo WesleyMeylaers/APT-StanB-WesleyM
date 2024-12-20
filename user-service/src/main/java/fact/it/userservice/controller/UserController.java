d689d570e1e0f871cb3a1fe0d6b14e1f38e984e5
WesleyMeylaers <r0929439@student.thomasmore.be>
2024-12-20 21:34:34 +0100

    additions user controller

--- user-service/src/main/java/fact/it/userservice/controller/UserController.java
+++ user-service/src/main/java/fact/it/userservice/controller/UserController.java
@@ -1,21 +1,4 @@
-package fact.it.userservice.controller;
-
-import fact.it.userservice.dto.UserResponse;
-import fact.it.userservice.service.UserService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-
-@RestController
-@RequestMapping("/api/users")
-@RequiredArgsConstructor
-public class UserController {
-
-    private final UserService userService;
-
-    // http://localhost:8081/api/users
+// http://localhost:8081/api/users
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
     public List<UserResponse> getAllUsers() {
@@ -28,4 +11,24 @@ public class UserController {
     public UserResponse getUserById(@PathVariable Long id) {
         return userService.getUserById(id);
     }
-}
+
+    // http://localhost:8081/api/users
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public UserResponse createUser(@RequestBody UserRequest userRequest) {
+        return userService.createUser(userRequest);
+    }
+
+    // http://localhost:8081/api/users/{id}
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public UserResponse updateUser(@PathVariable Long id, @RequestBody UserRequest userRequest) {
+        return userService.updateUser(id, userRequest);
+    }
+
+    // http://localhost:8081/api/users/{id}
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+    }
