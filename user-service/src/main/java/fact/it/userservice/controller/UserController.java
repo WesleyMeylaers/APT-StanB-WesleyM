522458d743f6ef3d12f1b74c723fdde905850974
WesleyMeylaers <r0929439@student.thomasmore.be>
2024-12-20 21:35:44 +0100

    small change

--- user-service/src/main/java/fact/it/userservice/controller/UserController.java
+++ user-service/src/main/java/fact/it/userservice/controller/UserController.java
@@ -1,4 +1,21 @@
-// http://localhost:8081/api/users
+package fact.it.userservice.controller;
+
+import fact.it.userservice.dto.UserResponse;
+import fact.it.userservice.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    // http://localhost:8081/api/users
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
     public List<UserResponse> getAllUsers() {
@@ -32,3 +49,4 @@
     public void deleteUser(@PathVariable Long id) {
         userService.deleteUser(id);
     }
+}
