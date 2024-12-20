dba18f99976c5534415712b47eb0112171ae94d2
WesleyMeylaers <r0929439@student.thomasmore.be>
2024-12-20 17:40:02 +0100

    oauth2

--- api-gateway/src/main/java/com/example/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/example/apigateway/ApiGatewayApplication.java
@@ -1,5 +1,6 @@
 package com.example.apigateway;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -8,7 +9,12 @@ import org.springframework.context.annotation.ComponentScan;
 public class ApiGatewayApplication {
 
     public static void main(String[] args) {
+        // Load .env variables
+        Dotenv dotenv = Dotenv.configure().load();
+
+        // Make environment variables available to Spring
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+
         SpringApplication.run(ApiGatewayApplication.class, args);
     }
-
 }
--- api-gateway/src/main/java/com/example/apigateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/example/apigateway/config/SecurityConfig.java
@@ -2,6 +2,8 @@ package com.example.apigateway.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
@@ -14,11 +16,13 @@ public class SecurityConfig {
     public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity serverHttpSecurity) {
         serverHttpSecurity
                 .authorizeExchange(exchange ->
-                        exchange.pathMatchers("/user", "/artist", "/album", "/playlist")
+                        exchange.pathMatchers(HttpMethod.GET,"/user", "/artist", "/album", "/playlist")
                                 .permitAll()  // Open toegang voor alle paden
                                 .anyExchange()
                                 .permitAll()  // Ook alle andere eindpunten open
-                );
+                )
+                .oauth2ResourceServer(oauth2->oauth2.jwt(Customizer.withDefaults()));
+
         return serverHttpSecurity.build();
     }
 }
